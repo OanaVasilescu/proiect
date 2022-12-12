558aad4f82218480f2ebedeb1a9bbf74b405b761
Oana Vasilescu <oana.vasilescu@sap.com>
2022-12-12 14:00:31 +0200

    init page 2

--- app/risk-app/webapp/controller/Overview.controller.js
+++ app/risk-app/webapp/controller/Overview.controller.js
@@ -1,5 +1,11 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (BaseController) {
     "use strict";
 
-    return BaseController.extend("riskapp.controller.Overview", {onInit() {}});
+    return BaseController.extend("riskapp.controller.Overview", {
+        onInit() {},
+        onItemSelect: function (oEvent) {
+            let oItem = oEvent.getParameter("item");
+            this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
+        }
+    });
 });
